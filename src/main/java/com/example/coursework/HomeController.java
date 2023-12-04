40b87f6c90b0607a2acc56619d64ae259e9e2445
Жанарбай <satkynovaz@gmail.com>
2023-12-04 13:30:37 +0600

    last

--- src/main/java/com/example/coursework/HomeController.java
+++ src/main/java/com/example/coursework/HomeController.java
@@ -8,6 +8,7 @@ import javafx.scene.control.TreeItem;
 import javafx.scene.control.TreeView;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
+import javafx.scene.text.FontWeight;
 import javafx.stage.Stage;
 
 import java.net.URL;
@@ -26,19 +27,19 @@ public class HomeController implements Initializable {
         TreeItem<String> menuRoot = new TreeItem<>("root");
 
         TreeItem<String> rootItemHome = new TreeItem<>("Home", new ImageView(new Image(getClass().getResourceAsStream("home.png"))));
-        TreeItem<String> rootItemMembers = new TreeItem<>("Members", new ImageView(new Image(getClass().getResourceAsStream("person.png"))));
-        TreeItem<String> rootItemStaff = new TreeItem<>("Staff", new ImageView(new Image(getClass().getResourceAsStream("person.png"))));
-        TreeItem<String> rootItemChat = new TreeItem<>("Chat", new ImageView(new Image(getClass().getResourceAsStream("person.png"))));
-        TreeItem<String> rootItemSchedule = new TreeItem<>("Schedule", new ImageView(new Image(getClass().getResourceAsStream("person.png"))));
-        TreeItem<String> rootItemPayments = new TreeItem<>("Payments", new ImageView(new Image(getClass().getResourceAsStream("person.png"))));
-        TreeItem<String> rootItemPrice = new TreeItem<>("Price", new ImageView(new Image(getClass().getResourceAsStream("pay.png"))));
+        TreeItem<String> rootItemMembers = new TreeItem<>("Members", new ImageView(new Image(getClass().getResourceAsStream("members.png"))));
+        TreeItem<String> rootItemStaff = new TreeItem<>("Staff", new ImageView(new Image(getClass().getResourceAsStream("staff.png"))));
+        TreeItem<String> rootItemChat = new TreeItem<>("Chat", new ImageView(new Image(getClass().getResourceAsStream("chat.png"))));
+        TreeItem<String> rootItemSchedule = new TreeItem<>("Schedule", new ImageView(new Image(getClass().getResourceAsStream("schedule.png"))));
+        TreeItem<String> rootItemPayments = new TreeItem<>("Payments", new ImageView(new Image(getClass().getResourceAsStream("payments.png"))));
+        TreeItem<String> rootItemPrice = new TreeItem<>("Price", new ImageView(new Image(getClass().getResourceAsStream("price.png"))));
 
         TreeItem<String> itemMembersAll = new TreeItem<>("All members", new ImageView(new Image(getClass().getResourceAsStream("person.png"))));
         TreeItem<String> itemMembersAdd = new TreeItem<>("Add a member", new ImageView(new Image(getClass().getResourceAsStream("person_add.png"))));
         TreeItem<String> itemMembersProfile = new TreeItem<>("Member's profile", new ImageView(new Image(getClass().getResourceAsStream("person.png"))));
 
-        TreeItem<String> itemScheduleClass = new TreeItem<>("Classes", new ImageView(new Image(getClass().getResourceAsStream("person.png"))));
-        TreeItem<String> itemSchedulePersonal = new TreeItem<>("Personal classes", new ImageView(new Image(getClass().getResourceAsStream("person.png"))));
+        TreeItem<String> itemScheduleClass = new TreeItem<>("Classes", new ImageView(new Image(getClass().getResourceAsStream("classes.png"))));
+        TreeItem<String> itemSchedulePersonal = new TreeItem<>("Personal classes", new ImageView(new Image(getClass().getResourceAsStream("personalClasses.png"))));
 
         menuRoot.getChildren().addAll(rootItemHome, rootItemMembers, rootItemStaff, rootItemChat, rootItemSchedule, rootItemPayments, rootItemPrice);
         rootItemMembers.getChildren().addAll(itemMembersAll, itemMembersAdd, itemMembersProfile);
